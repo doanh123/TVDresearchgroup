5e497758538eef810bd517fa80c44ae71a4dc03d
Huan Do <doboy0@gmail.com>
2013-10-24 21:18:45 -0700

    set up initial routes

--- hello.py
+++ hello.py
@@ -1,9 +1,30 @@
 import os
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/membership')
+def membership():
+    return render_template('membership.html')
+
+@app.route('/publication')
+def publication():
+    return render_template('publication.html')
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
 
