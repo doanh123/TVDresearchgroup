44350479e246b28c836968def7bb50594316fa1e
doanh123 <doanh@intellievents.com>
2013-10-26 07:01:22 +0000

    fixed /index

--- hello.py
+++ hello.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/index')
+def mainpage():
+    return render_template('indx.html')
+
 @app.route('/about')
 def about():
     return render_template('about.html')
